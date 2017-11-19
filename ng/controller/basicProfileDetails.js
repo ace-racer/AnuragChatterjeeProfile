76d5038353c74c29dbd0c5917a30814b098c5e52
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2017-11-19 15:38:56 +0530

    Update left navigation

--- ng/controller/basicProfileDetails.js
+++ ng/controller/basicProfileDetails.js
@@ -3,12 +3,11 @@ app.controller('basicProfileDetailsCtrl', function ($scope, $http) {
     $scope.firstName = "Anurag";
     $scope.lastName = "Chatterjee";
     $scope.fullName = $scope.firstName + " " + $scope.lastName;
-    $scope.currentYear = new Date().getFullYear();
-    $scope.sectionDetailsResponse = null;   
-
+    $scope.currentYear = new Date().getFullYear();       
     $http.get('../../ProfileData/sections.json').then(function (res) {
         debugger;
-        $scope.sectionDetailsResponse = res.data;
+        $scope.sectionDetails = res.data;
+        $scope.$watch('sectionDetails');
         //$timeout(function () {
             
         //})
@@ -25,11 +24,5 @@ app.controller('basicProfileDetailsCtrl', function ($scope, $http) {
         //}
     }).catch(function (data) {
         console.log("An error occurred while trying to find the sections file");
-    });
-
-    $scope.$watch('sectionDetailsResponse', function () {
-        alert("value updated!! ");
-        $scope.sectionDetails = $scope.sectionDetailsResponse;
-    })
-
+    });   
 });
