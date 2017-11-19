10d275e888cb1e5991a9f51af30cb97ef548b10f
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2017-11-19 15:07:47 +0530

    Move code to get section details in basic profile controller

--- ng/controller/basicProfileDetails.js
+++ ng/controller/basicProfileDetails.js
@@ -1,7 +1,35 @@
 var app = angular.module('profileApp', []);
-app.controller('basicProfileDetailsCtrl', function ($scope) {
+app.controller('basicProfileDetailsCtrl', function ($scope, $http) {
     $scope.firstName = "Anurag";
     $scope.lastName = "Chatterjee";
     $scope.fullName = $scope.firstName + " " + $scope.lastName;
     $scope.currentYear = new Date().getFullYear();
+    $scope.sectionDetailsResponse = null;   
+
+    $http.get('../../ProfileData/sections.json').then(function (res) {
+        debugger;
+        $scope.sectionDetailsResponse = res.data;
+        //$timeout(function () {
+            
+        //})
+        //if (res) {
+        //    //$timeout(function () {
+        //    //    debugger;
+        //    //    $scope.sectionDetails = res.data;
+        //    //}, 1000)
+        //    debugger;
+        //    $scope.sectionDetails = res.data;
+        //    //                $scope.$apply();
+        //} else {
+        //    console.log("There is no response received");
+        //}
+    }).catch(function (data) {
+        console.log("An error occurred while trying to find the sections file");
+    });
+
+    $scope.$watch('sectionDetailsResponse', function () {
+        alert("value updated!! ");
+        $scope.sectionDetails = $scope.sectionDetailsResponse;
+    })
+
 });
