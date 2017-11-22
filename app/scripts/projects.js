7604fbe6e62c1e6b5d501a609b1f24bfcdd0fbf9
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2017-11-22 23:51:29 +0530

    Update files - projects, footer and data

--- app/scripts/projects.js
+++ app/scripts/projects.js
@@ -5,6 +5,11 @@ angular.module('profileApp')
         dataSvc.getProjects().then(function (res) {
             if (res) {
                 var projectDetailsObj = res.data;
+                var projectDetails = projectDetailsObj.details;
+                for (var itr = 0; itr < projectDetails.length; itr++) {
+                    projectDetails[itr].monthName = $scope.getMonthNameFromNumber(projectDetails[itr].month);
+                }
+
                 $scope.projectDetails = projectDetailsObj.details;
             } else {
                 console.log("There was no response while getting the JSON")
@@ -12,5 +17,18 @@ angular.module('profileApp')
         }).catch(function (data) {
             console.log("An error occurred while trying to find the project details file");
         });
+
+        /*
+            Get the month name from the month number         
+         */
+        $scope.getMonthNameFromNumber = function (monthNumber) {            
+            if (monthNumber > 0 && monthNumber < 13)
+            {
+                // array Index starts from 0
+                var arrayIndex = monthNumber - 1;
+                var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                return monthNames[arrayIndex];
+            }
+        }
 
     }]);
