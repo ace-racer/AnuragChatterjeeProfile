a9de53e35def1747291cff6ea4a774fc66a653e7
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2017-11-26 18:32:49 +0530

    Add search functionality in projects

--- app/scripts/projects.js
+++ app/scripts/projects.js
@@ -1,6 +1,6 @@
 angular.module('profileApp')
     .controller('projectsCtrl', ['$scope', '$location', 'dataSvc', function ($scope, $location, dataSvc) {
-        $scope.highlights = [];
+        $scope.allProjectDetails = [];
 
         dataSvc.getProjects().then(function (res) {
             if (res) {
@@ -11,6 +11,9 @@ angular.module('profileApp')
                 }
 
                 $scope.projectDetails = projectDetailsObj.details;
+                for (var itr = 0; itr < projectDetails.length; itr++) {
+                    $scope.allProjectDetails.push(projectDetails[itr]);
+                }
             } else {
                 console.log("There was no response while getting the JSON")
             }
@@ -21,14 +24,48 @@ angular.module('profileApp')
         /*
             Get the month name from the month number         
          */
-        $scope.getMonthNameFromNumber = function (monthNumber) {            
-            if (monthNumber > 0 && monthNumber < 13)
-            {
+        $scope.getMonthNameFromNumber = function (monthNumber) {
+            if (monthNumber > 0 && monthNumber < 13) {
                 // array Index starts from 0
                 var arrayIndex = monthNumber - 1;
                 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
                 return monthNames[arrayIndex];
             }
+        }
+
+        /*
+         Get the projects with the provided text
+         */
+        $scope.searchItemWithText = function () {
+            debugger;
+            text = $scope.filterText;
+            var minimumCharactersForSearch = 1;
+            if (text && text.length > minimumCharactersForSearch) {
+                var projectDetails = $scope.allProjectDetails;
+                var projectDetailsToShow = [];
+
+                // for each project details
+                for (var itr = 0; itr < projectDetails.length; itr++) {
+                    // for each tag in the project details
+                    for (var jtr = 0; jtr < projectDetails[itr].tags.length; jtr++) {
+                        var textLower = text.toLowerCase();
+                        var tagItemLower = projectDetails[itr].tags[jtr].toLowerCase();
+
+                        // if a portion of any tag matches the text in case ignorant search
+                        if(tagItemLower.indexOf(textLower) >= 0)
+                        {
+                            // Add the item only once
+                            projectDetailsToShow.push(projectDetails[itr]);
+                            break;
+                        }
+                    }
+                }
+            } else {
+                // add all items if there is no search text
+                projectDetailsToShow = $scope.allProjectDetails;
+            }
+
+            $scope.projectDetails = projectDetailsToShow;
         }
 
     }]);
